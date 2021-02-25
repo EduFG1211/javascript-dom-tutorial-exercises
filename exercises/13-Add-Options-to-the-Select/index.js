ca7eeab8b1defbfa8a0fab32dccacabc54d23ae7
Edventures <edufg13@gmail.com>
2021-02-25 23:40:15 +0000

    completo hsta p13

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -3,25 +3,42 @@ window.onload = function() {
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
 	// your code here
-    let elem = document.querySelector("#mySelect");
-	let op0 = document.createElement("option");
-	let op1 = document.createElement("option");
-	let op2 = document.createElement("option");
-	let op3 = document.createElement("option");
-	let op4 = document.createElement("option");
-	let op5 = document.createElement("option");
-	let op6 = document.createElement("option");
-	op0.innerHTML = countries[0];
-	op1.innerHTML = countries[1];
-	op2.innerHTML = countries[2];
-	op3.innerHTML = countries[3];
-	op4.innerHTML = countries[4];
-	op5.innerHTML = countries[5];
-	op6.innerHTML = countries[6];
-	elem.appendChild(op1);
-	elem.appendChild(op2);
-	elem.appendChild(op3);
-	elem.appendChild(op4);
-	elem.appendChild(op5);
-	elem.appendChild(op6);
+	let list = document.querySelector("#mySelect");
+	for (let i = 0; i < countries.length; i++) {
+		let opt = document.createElement("option");
+		opt.innerHTML = countries[i];
+		list.appendChild(opt);
+	}
+	let childs = list.childNodes;
+	list.addEventListener("change", function() {
+		for (let i = 0; i < childs.length; i++) {
+			if (childs[i].selected === true) {
+				switch (i) {
+					case 3:
+						alert("USA"); // code block
+						break;
+					case 4:
+						alert("France"); // code block
+						break;
+					case 5:
+						alert("Italy"); // code block
+						break;
+					case 6:
+						alert("Brazil"); // code block
+						break;
+					case 7:
+						alert("Colombia"); // code block
+						break;
+					case 8:
+						alert("Belize"); // code block
+						break;
+					case 9:
+						alert("Venezuela"); // code block
+						break;
+					default:
+						alert("Select your country"); // code block
+				}
+			}
+		}
+	});
 };
--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,24 @@
 // Your code here
+let addbutton = document.createElement("button");
+addbutton.innerHTML = "Add Task";
+let location = document.querySelector("body");
+location.appendChild(addbutton);
+
+addbutton.addEventListener("click", function() {
+	let addli = document.createElement("li");
+	let inputValue = document.querySelector("#addToDo").value;
+	let text = document.createTextNode(inputValue);
+	addli.innerHTML = '<span><i class="fa fa-trash"></i></span> ';
+	addli.appendChild(text);
+	let myUL = document.querySelector("ul");
+	myUL.appendChild(addli);
+});
+
+// let addli = document.createElement("li");
+// let inputValue = "Ejemplo agregar linea";
+// //let inputValue = document.querySelector("#addToDo").value;
+// let text = document.createTextNode(inputValue);
+// addli.innerHTML = '<span><i class="fa fa-trash"></i></span> ';
+// addli.appendChild(text);
+// let myUL = document.querySelector("ul");
+// myUL.appendChild(addli);
