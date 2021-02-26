8dbb18239b3bc5e259b9904ee30a249b642aec62
Edventures <edufg13@gmail.com>
2021-02-26 00:39:07 +0000

    me falta el p14

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -4,21 +4,37 @@ addbutton.innerHTML = "Add Task";
 let location = document.querySelector("body");
 location.appendChild(addbutton);
 
+//Crea nuevas variables
+
 addbutton.addEventListener("click", function() {
+	let myUL = document.querySelector("ul");
 	let addli = document.createElement("li");
 	let inputValue = document.querySelector("#addToDo").value;
 	let text = document.createTextNode(inputValue);
 	addli.innerHTML = '<span><i class="fa fa-trash"></i></span> ';
 	addli.appendChild(text);
-	let myUL = document.querySelector("ul");
-	myUL.appendChild(addli);
+	if (inputValue === "") {
+		alert("You must write something!");
+	} else {
+		myUL.appendChild(addli);
+	}
+	document.querySelector("#addToDo").value = "";
+	alert(myUL.childNodes.length);
 });
 
-// let addli = document.createElement("li");
-// let inputValue = "Ejemplo agregar linea";
-// //let inputValue = document.querySelector("#addToDo").value;
-// let text = document.createTextNode(inputValue);
-// addli.innerHTML = '<span><i class="fa fa-trash"></i></span> ';
-// addli.appendChild(text);
-// let myUL = document.querySelector("ul");
-// myUL.appendChild(addli);
+let close = document.getElementsByClassName("fa fa-trash");
+//let childs = myUL.childNodes;
+//let span = document.create
+for (let i = 0; i < close.length; i++) {
+	close[i].onclick = function() {
+		let myUL2 = document.querySelector("ul");
+		let childs = myUL2.childNodes;
+		let div = childs[2 * i + 1];
+		div.style.display = "none";
+		alert(myUL2.childNodes.length); // let div = this.parentElement;
+		// div.style.display = "none";
+		// let list = document.querySelector("ul");
+		// let childs = myUL.childNodes;
+		// myUL.removeChild(childs[i * 2 + 1]);
+	};
+}
